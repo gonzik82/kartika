5e2e16235c0ef3cd22aacee07a6f01f0bf0d1d42
Pavel Elfimov <pelfimov@ya.ru>
2017-08-15 17:55:21 +0700

    Начал переделку с препроцессорами

---
+++ Gruntfile.js
@@ -0,0 +1,134 @@
+"use strict";
+
+module.exports = function(grunt) {
+  require("load-grunt-tasks")(grunt);
+
+  grunt.initConfig({
+    less: {
+      style: {
+        files: {
+          "build/css/style.css": "less/style.less"
+        }
+      }
+    },
+
+    postcss: {
+      style: {
+        options: {
+          processors: [
+            require("autoprefixer")({browsers: [
+              "last 2 versions"
+            ]}),
+            require("css-mqpacker")({
+              sort: true
+            })
+          ]
+        },
+        src: "build/css/*.css"
+      }
+    },
+
+    csso: {
+      style: {
+        options: {
+          report: "gzip"
+        },
+        files: {
+          "build/css/style.min.css": ["build/css/style.css"]
+        }
+      }
+    },
+
+    imagemin: {
+      images: {
+        options: {
+          optimizationLevel: 3,
+          progressive: true
+        },
+        files: [{
+          expand: true,
+          src: ["build/img/**/*.{png,jpg,gif}"]
+        }]
+      }
+    },
+
+    browserSync: {
+      server: {
+        bsFiles: {
+          src: [
+            "build/*.html",
+            "build/css/*.css",
+            "build/js/*.js"
+
+          ]
+        },
+        options: {
+          server: "build/",
+          watchTask: true,
+          notify: false,
+          open: true,
+          cors: true,
+          ui: false
+        }
+      }
+    },
+
+    watch: {
+      html: {
+        files: ["*.html"],
+        tasks: ["copy:html"]
+      },
+      js: {
+        files: ["js/*.js"],
+        tasks: ["copy:js"]
+      },
+      style: {
+        files: ["less/**/*.less"],
+        tasks: ["less", "postcss", "csso"]
+      }
+    },
+
+    copy: {
+      build: {
+        files: [{
+          expand: true,
+          src: [
+            "fonts/**/*.{woff,woff2}",
+            "img/**",
+            "js/**",
+            "*.html"
+          ],
+          dest: "build"
+        }]
+      },
+      html: {
+        files: [{
+          expand: true,
+          src: ["*.html"],
+          dest: "build"
+        }]
+      },
+      js: {
+        files: [{
+          expand: true,
+          src: ["js/*.js"],
+          dest: "build"
+        }]
+      }
+    },
+
+    clean: {
+      build: ["build"]
+    }
+  });
+
+  grunt.registerTask("serve", ["browserSync", "watch"]);
+  grunt.registerTask("build", [
+    "clean",
+    "copy",
+    "less",
+    "postcss",
+    "csso",
+    "imagemin"
+  ]);
+};
