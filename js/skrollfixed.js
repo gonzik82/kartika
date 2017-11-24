6a3f20dd03d94381ababb4fb39ce7e56da799297
Pavel Elfimov <pelfimov@ya.ru>
2017-11-24 17:30:47 +0700

    Настроил главное меню для фиксирования вверху страницы

---
+++ js/skrollfixed.js
@@ -0,0 +1,10 @@
+jQuery(window).scroll(function() {
+    var the_top = jQuery(document).scrollTop();
+    console.log("Скрол",the_top);
+    if (the_top > 667) {
+        jQuery('.main-navigation').addClass('main-navigation--fixed');
+    }
+    else {
+        jQuery('.main-navigation').removeClass('main-navigation--fixed');
+    }
+});
