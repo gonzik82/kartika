c0d3ca7e14543f6def7dc619815d99a9fc1f83b4
Павел <pelfimov@ya.ru>
2017-12-04 21:50:41 +0700

    Настроил включение меню по кнопки

--- js/popup.js
+++ js/popup.js
@@ -1,78 +1,43 @@
 var link = document.querySelector(".main-header__contact-btn");
 var index;
 var popup = document.querySelector(".request-call");
-var closes = document.querySelectorAll(".btn-close"),
+var user = popup.querySelector("[name=request-call-user-name]");
+var closes = popup.querySelector(".btn-close"),
   close;
-var user = popup.querySelector("[name=user]");
-var tel = popup.querySelector("[name=phone]");
-var checkbox = popup.querySelector("[name=personal-data]");
-var consultationform = document.querySelector(".order-form--consultation");
-
 
 
 link.addEventListener("click", function(event) {
   event.preventDefault();
-  popup.classList.add("modal-content--show");
+  popup.classList.add("request-call--show");
   user.focus();
-  if (popupOrder.classList.contains("modal-content--show")) {
-    popupOrder.classList.remove("modal-content--show");
 
-  }
 });
 
-for (index = 0; index < linkOrders.length; index++) {
-  linkOrder = linkOrders[index];
-  linkOrder.addEventListener('click', clickHandler);
+$(document).click( function(event){
+	if( $(event.target).closest(".request-call__wrapper").length )
+	return;
+  if ($(".request-call").hasClass("request-call--show")) {
+    $(".request-call").removeClass("request-call--show");
+  };
+	event.stopPropagation();
+});
 
-}
+$('.main-header__contact-btn').click( function() {
+		return false;
+	});
 
-
-function clickHandler(event) {
-  console.log('click', this.innerText);
+closes.addEventListener("click", function(event) {
   event.preventDefault();
-  popupOrder.classList.add("modal-content--show");
-  user.focus();
-  if (popup.classList.contains("modal-content--show")) {
-    popup.classList.remove("modal-content--show");
-
+  if (popup.classList.contains("request-call--show")) {
+    popup.classList.remove("request-call--show");
   }
-
-}
-
-for (index = 0; index < closes.length; index++) {
-  close = closes[index];
-  close.addEventListener('click', clickClose);
-}
-
-
-function clickClose(event) {
-  console.log('click', this.innerText);
-  event.preventDefault();
-  if (popup.classList.contains("modal-content--show")) {
-    popup.classList.remove("modal-content--show");
-
-  }
-  if (popupOrder.classList.contains("modal-content--show")) {
-    popupOrder.classList.remove("modal-content--show");
-  }
-  if (popupthenks.classList.contains("modal-content--show")) {
-    popupthenks.classList.remove("modal-content--show");
-  }
-}
-
+});
 
 
 window.addEventListener("keydown", function(event) {
   if (event.keyCode === 27) {
-    if (popup.classList.contains("modal-content--show")) {
-      popup.classList.remove("modal-content--show");
+    if (popup.classList.contains("request-call--show")) {
+      popup.classList.remove("request-call--show");
     }
-    if (popupOrder.classList.contains("modal-content--show")) {
-      popupOrder.classList.remove("modal-content--show");
-    }
-    if (popupthenks.classList.contains("modal-content--show")) {
-      popupthenks.classList.remove("modal-content--show");
-    }
-
   }
 });
