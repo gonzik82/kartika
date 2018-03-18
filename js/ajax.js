430769afa210cfe84f3e30433e21df4c8dc411e1
Павел <pelfimov@ya.ru>
2018-03-18 15:01:03 +0700

    осталась проблемма с ответственным за клиента при передачи карточки в ЦРМ

--- js/ajax.js
+++ js/ajax.js
@@ -6,26 +6,19 @@ function callorder(form){
     var $form = $(form); // jquery объект формы
     var data = $form.serialize(); //собираем данные с формы в виде querystring
     var m_method=$form.attr('method');
-    var n;
-    n=0;
     console.log(m_method);
-    n=n+1;
-    console.log(n);
-    //получаем адрес скрипта на сервере, куда нужно отправить форму
     var m_action=$form.attr('action');
     console.log(m_action);
-    //получаем данные, введенные пользователем в формате input1=value1&input2=value2...,
-    //то есть в стандартном формате передачи данных формы
     var m_data=$form.serialize();
     console.log(m_data);
-    // $.ajax({
-    // type: m_method,
-    // url: m_action,
-    // data: m_data,
-    // success: function(result){
-    // $('#test_form').html(result);
-    // }
-    // });
+    $.ajax({
+    type: m_method,
+    url: m_action,
+    data: m_data,
+    success: function(result){
+    $('#test_form').html(result);
+    }
+    });
 
   $(".thanks-call").addClass("thanks-call--show");
   $(".thanks-call__wrapper").addClass("thanks-call__wrapper--show");
