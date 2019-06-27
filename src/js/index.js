8bcae39be18f3fa0b9c69048b2e93a5bcfb82456
Pavel <pelfimov@yandex.ru>
2019-06-27 18:01:15 +0700

    to do

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,8 @@
 // Main js file
 
 // another js file (example)
-import './lib/ajax.js'
-import './service/filelayout.js'
+import './callorder.js'
+// import './service/filelayout.js'
 import './service/move.js'
 import './service/timer.js'
 import './service/skrollfixed.js'
--- src/js/service/form-load.js
+++
@@ -1,4 +0,0 @@
-const btn = document.querySelector(".btn--request-call");
-btn.addEventListener(`click` , (ev) =>{
-  console.log(ev)
-})
